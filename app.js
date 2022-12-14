c7cbc662d55c029ca1e3c0ad2691cb2ae582e007
Jorge Mendonca Boaretto Loch <jorgembloch2001@gmail.com>
2022-12-14 20:45:54 -0300

    adding and connecting to database

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+//connection with mongoDB string -> mongodb+srv://user_db:njB6sH0zRaRI291Q@clustersofteam.iqsjr3x.mongodb.net/?retryWrites=true&w=majority
+
+const url = "mongodb+srv://user_db:njB6sH0zRaRI291Q@clustersofteam.iqsjr3x.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(url);
+mongoose.set('strictQuery', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Error trying to connect with database: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('This aplication has been disconnected from the database!')
+})
+
+mongoose.connection.on('connected', () => console.log('Application has been connected to database'))
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const indexRoute = require('./routes/index');
 const collaboratorRoute = require('./routes/collaboratorRoutes');
