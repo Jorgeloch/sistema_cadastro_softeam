f08a1583b8c088830750a230487f9e143f4a7dbe
Jorge Mendonca Boaretto Loch <jorgembloch2001@gmail.com>
2022-12-17 11:45:57 -0300

    setting up the first routes of projects

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Projects = require('../models/project')
 
 router.get('/', (request, response) => {
-    return response.send({message: "tudo ok com o metodo GET da rota de projetos"});
+    Projects.find({}, (err, data) => {
+        if (err) return response.send({ error: `Error trying to connect to the database: ${err}` });
+        return response.send(data);
+    })
 });
 
-router.post('/', (request, response) => {
-    return response.send({message: "tudo ok com o metodo POST da rota de projetos"});
+router.post('/create', (request, response) => {
+    const { id, name, description, status, value, collaborators, companies } = request.body;
+
+    if (!id || !name || !description || !status || !value || !collaborators || !companies) return response.send({ error: "Insufficient Data!" });
+
+    Projects.findOne({id}, (err, data) => {
+        if (err) return response.send({ error: `Error trying to find project: ${err}` });
+        if (data) return response.send({ error: 'Project already exist!' });
+
+        Projects.create(request.body, (err, data) => {
+            if (err) return response.send({ error: `Error trying to create project: ${err}`});
+            data.password = undefined;
+            return data;
+        });
+    });
 });
 
 module.exports = router;
