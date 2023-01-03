08d4446c1ba0ae23c4cdd2a25eea3d0535c712cc
Jorge Mendonca Boaretto Loch <jorgembloch2001@gmail.com>
2023-01-03 15:44:39 -0300

    fixing problems with authUpdate middleware

--- middlewares/authUpdate.js
+++ middlewares/authUpdate.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config-data/config');
 const Collaborators = require('../models/collaborator');
 
-const authUpdate = (request, response, next) => {
+const authUpdate = async (request, response, next) => {
     const {_id} = request.params;
     const authHeader = request.headers.authorization;
 
@@ -13,13 +13,14 @@ const authUpdate = (request, response, next) => {
     }
     else{
         const token = authHeader.substring(7, authHeader.length);
-        const collaborator = Collaborators.findById(_id);
+        const collaborator = await Collaborators.findById(_id);
 
         jwt.verify(token, config.JWTPassword, (err, decoded) => {
             if (err) return response.status(401).send({ error: `Error trying to verify token: ${err}`});
             if(decoded.id != collaborator._id) return response.status(401).send({ error: "unauthorized to modify this collaborator's password" });
 
             response.locals.authData = decoded;
+            
             return next();
         });
     }
