22fefb7942ca381a210e4acf0b373668ac78ffbd
Jorge Mendonca Boaretto Loch <jorgembloch2001@gmail.com>
2023-01-02 21:29:34 -0300

    fixing some parts of the documentation/ fixing the way the API read the token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,14 +2,22 @@ const jwt = require('jsonwebtoken');
 const config = require('../config-data/config')
 
 const auth = (request, response, next) => {
-    const tokenHeader = request.headers.auth;
+    const authHeader = request.headers.authorization;
 
-    if(!tokenHeader) return response.status(401).send({ error:"Token wasn't sent" });
-    jwt.verify(tokenHeader, config.JWTPassword, (err, decoded) => {
-        if (err) return response.status(401).send({ error: `Error trying to verify token: ${err}`});
-        response.locals.authData = decoded;
-        return next();
-    });
+    if(!authHeader) return response.status(401).send({ error:"Token wasn't sent" });
+
+    if(!authHeader.startsWith("Bearer ")){
+        return response.status(401).send({ error:"Token wasn't sent" });
+    }
+    else{
+        const token = authHeader.substring(7, authHeader.length);
+
+        jwt.verify(token, config.JWTPassword, (err, decoded) => {
+            if (err) return response.status(401).send({ error: `Error trying to verify token: ${err}`});
+            response.locals.authData = decoded;
+            return next();
+        });
+    }
 } 
 
 module.exports = auth;
